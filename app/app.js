9e34d3a9b46317f24ea600319eb0cb42fe5e663a
qubaish <qbhatti143@gmail.com>
2020-04-05 11:39:23 +0400

    Add tavis changes

--- app/app.js
+++ app/app.js
@@ -18,13 +18,6 @@ import 'sanitize.css/sanitize.css';
 
 // Import root app
 import App from 'containers/App';
-
-// Load the favicon and the .htaccess file
-/* eslint-disable import/no-unresolved, import/extensions */
-import '!file-loader?name=[name].[ext]!./images/favicon.ico';
-import 'file-loader?name=.htaccess!./.htaccess';
-/* eslint-enable import/no-unresolved, import/extensions */
-
 import configureStore from './configureStore';
 
 // Create redux store with history
